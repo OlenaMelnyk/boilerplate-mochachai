b8fd82a7ca9fa18a493e7daf8d4184718c4b5a4f
olena <melolena33@gmail.com>
2020-02-05 11:24:53 +0200

    Correction for approximate test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -105,7 +105,7 @@ suite('Unit Tests', function(){
     // it should be less than 1
     test('#approximately', function() {
       assert.approximately(weirdNumbers(0.5) , 1, 0.5 );
-      assert.approximately(weirdNumbers(0.2) , 1, 0.7 );
+      assert.approximately(weirdNumbers(0.2) , 1, 0.8 );
     });
   });
 
